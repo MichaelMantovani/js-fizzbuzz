3ae1ada4d9dff41c3119e363c8b5042c4fc3173d
MichaelMantovani <michmantovani@gmail.com>
2023-05-30 16:33:44 +0200

    Add Js cycle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log('JS OK')
+
+let numberText = ''
+
+for (let i = 1; i <= 100; i++) { 
+  if(i % 5 === 0 && i % 3 === 0){
+  numberText += ' BuzzFizz ';
+} else if (i % 3 === 0){
+  numberText += ' Fizz ';
+} else if (i % 5 === 0){
+  numberText += ' Buzz '
+} else {
+  numberText += ' ' + i;
+}
+}
+
+
+
+console.log(numberText);
